899e48bd0e496723c07c9e3b8faf68971ff4ffe1
Andrew Virya Victorio <andrew.avv03@gmail.com>
2023-12-17 21:51:56 +0700

    use aside tag

--- src/app/components/ui/SidebarMenu.tsx
+++ src/app/components/ui/SidebarMenu.tsx
@@ -14,49 +14,51 @@ import React from "react"
 
 const DefaultSidebar = () => {
 	return (
-		<Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-			<Link
-				href="https://team37.co"
-				className="flex justify-center pb-2"
-				target="_blank"
-			>
-				<Image
-					src="/logo.png"
-					height={150}
-					width={200}
-					alt="Team 37 logo"
-				/>
-			</Link>
-			<List>
-				<ListItem>
-					<ListItemPrefix>
-						<PresentationChartBarIcon height={24} width={24} />
-					</ListItemPrefix>
-					Dashboard
-				</ListItem>
-				<ListItem>
-					<ListItemPrefix>
-						<DocumentTextIcon height={24} width={24} />
-					</ListItemPrefix>
-					My Course
-				</ListItem>
-				<ListItem>
-					<ListItemPrefix>
-						<AcademicCapIcon height={24} width={24} />
-					</ListItemPrefix>
-					Certificate
-				</ListItem>
-				<ListItem>
-					<ListItemPrefix>
-						<ChatBubbleBottomCenterTextIcon
-							height={24}
-							width={24}
-						/>
-					</ListItemPrefix>
-					Discussion
-				</ListItem>
-			</List>
-		</Card>
+		<aside>
+			<Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+				<Link
+					href="https://team37.co"
+					className="flex justify-center pb-2"
+					target="_blank"
+				>
+					<Image
+						src="/logo.png"
+						height={150}
+						width={200}
+						alt="Team 37 logo"
+					/>
+				</Link>
+				<List>
+					<ListItem>
+						<ListItemPrefix>
+							<PresentationChartBarIcon height={24} width={24} />
+						</ListItemPrefix>
+						Dashboard
+					</ListItem>
+					<ListItem>
+						<ListItemPrefix>
+							<DocumentTextIcon height={24} width={24} />
+						</ListItemPrefix>
+						My Course
+					</ListItem>
+					<ListItem>
+						<ListItemPrefix>
+							<AcademicCapIcon height={24} width={24} />
+						</ListItemPrefix>
+						Certificate
+					</ListItem>
+					<ListItem>
+						<ListItemPrefix>
+							<ChatBubbleBottomCenterTextIcon
+								height={24}
+								width={24}
+							/>
+						</ListItemPrefix>
+						Discussion
+					</ListItem>
+				</List>
+			</Card>
+		</aside>
 	)
 }
 
